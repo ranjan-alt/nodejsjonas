8d7636614d839efa707c31f872320b430877e03b
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-07-14 07:50:30 +0000

    MAIN

--- nodejonas/app.js
+++ nodejonas/app.js
@@ -1,24 +1,20 @@
+///////
+////
+///
+// now after learning.js we will refactor the code to improve the look and feel of our code 
+
+
 const express = require("express");
 const fs = require("fs")
 
 
 const app = express();
-app.use(express.json()) //while create a post request we have to create a middleware and this express.json is a middelware
-//A middleware is just the function that can modify the incoming request data , it stands middle of request from client and response from server
-
-
-
-// app.get("/", (req, res) => {
-//     res.status(200).send("hello from the server side")
-// })
+app.use(express.json())
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-//JSON.parse will automatically convert json into array of javascript objects
 
 
-//GET METHOD
-app.get("/api/v1/tours", (req, res) => {     // create your own version of api we must always specify the version of the api
-    //here res req is the route handler
+const getAllTours = (req, res) => {
     res.status(200).json({
         status: "success",
         results: tours.length,
@@ -26,17 +22,12 @@ app.get("/api/v1/tours", (req, res) => {     // create your own version of api w
             tours
         }
     })
-})
+}
 
-//GET Single tours URL Parameters
-app.get("/api/v1/tours/:id", (req, res) => {
-    console.log(req.params)   //req.params is a really nice object that assigns value to our variable or the parameter we define
-
-    const id = req.params.id * 1;  // since id is a string to convert it to number we just have to multiply with the number
-    const tour = tours.find(el => el.id === id)   // this callback with find method will return true or false and find will create an array where the id is matched
-    //find is a reqular js function which can be used on arrays 
-    //here we pass a callback function it will bascically loop through each of the array and with each of the iteration we will have access to current element and it will return true or false in each iteration
-    // then find will create ana array which will only create an elements where this comapraison will be true
+const getTour = (req, res) => {
+    console.log(req.params)
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id)
     if (id > tours.length) {
         return res.status(404).json({
             status: "failed",
@@ -50,26 +41,14 @@ app.get("/api/v1/tours/:id", (req, res) => {
             tour
         }
     })
-})
-
-//POST METHOD
-app.post("/api/v1/tours", (req, res) => {    //the method will only change but the URL remains same and we can get data from client to the server
-    //req is an object which stores all the data and information about the request that was perfomed
-    //express doesnot put the body data on the request so in order to have the data available we have to use something called middleware
-    // for post request to be performed we are using middleware i.e ----->>>>> app.use(express.json())
-
-    // console.log(req.body) //just doing console here req.body will print json which we gave in postman
-
-    //now task is to store data in our fictional database
-    //firstof all we need to figureout the new id 
-    const newId = tours[tours.length - 1].id + 1   // we want to get the last one so we get by tours.length -1 and + 1to add new id
+}
 
 
+const createTour = (req, res) => {
+    const newId = tours[tours.length - 1].id + 1
     const newTour = Object.assign({ id: newId }, req.body)
-    //object.assign bascially allows us to add object to the exisitng object together veryyyyyyy important 
-    //next step is to push the newTour to the tour array 
     tours.push(newTour);
-    // as now have push but we need to persist this in the new file 
+
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
             status: "success",
@@ -78,16 +57,53 @@ app.post("/api/v1/tours", (req, res) => {    //the method will only change but t
             }
         })
     })
-    //here tours at the end because this is the place where we have to write and must be stringified
 
-    //question shall we write writefile or writefilesync ?? [Event Loop Lecture]
-    // the answer is writefile as we are in callback function which will run in the event loop and we can never even block the event loop
-    // so we need writefile and not the synchronous one
+}
 
-    // res.send("done") 
-    //sending the response and it must be done to complete the cycle of req, res
-})
+const updateTour = (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID"
+        })
+    }
+    res.status(200).json({
+        status: "sucess",
+        data: {
+            tour: "<Updated tour here ....>"
+        }
+    })
+}
 
+const deleteTour = (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(204).json({
+            status: "fail",
+            message: "Invalid ID"
+        })
+    }
+    res.status(200).json({
+        status: "sucess",
+        data: {
+            tour: null
+        }
+    })
+}
+
+
+// app.get("/api/v1/tours", getAllTours)
+// app.get("/api/v1/tours/:id", getTour)
+// app.post("/api/v1/tours", createTour)
+// app.patch("/api/v1/tours/:id", updateTour)
+// app.delete("/api/v1/tours/:id", deleteTour)
+
+
+// But still code is not perfect we can do more perfect way by routes 
+//instead of writing single way we can write the same code in one line 
+
+app.route("/api/v1/tours").get(getAllTours).post(createTour)
+
+app.route("/api/v1/tours/:id").get(getTour).patch(updateTour).delete(deleteTour)
 
 const port = 3000;
 app.listen(port, () => {
