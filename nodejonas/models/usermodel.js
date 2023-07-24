6d72e41d753de14fbe87a6af9361c3984264e54b
Ranjan <ranjan.kumar@publicissapient.com>
2023-07-24 18:48:48 +0530

    brcypt

--- nodejonas/models/usermodel.js
+++ nodejonas/models/usermodel.js
@@ -1,6 +1,7 @@
 //step 1 first we require mongoose package
 const mongoose = require("mongoose");
 // const validator = require("validator")
+const bcrypt = require("bcryptjs")
 
 
 // name email foto password passwordconfirm
@@ -10,7 +11,21 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: [true, "please provide your email"], unique: true, lowercase: true },
     Photo: { type: String },
     password: { type: String, required: [true, "Please provide a password"], minLength: 8 },
-    passwordConfirm: { type: String, required: [true, "Please confrim your password"] }
+    passwordConfirm: {
+        type: String, required: [true, "Please confrim your password"],
+        // this only works on SAVE!!!
+        validate: {
+            validator: function (el) {
+                return el === this.password
+            },
+            message: "password is not same"
+        }
+    }
+})
+
+userSchema.pre("save", function (next) {
+    if (!this.isModified("password")) return next()
+
 })
 
 const User = mongoose.model("User", userSchema)
