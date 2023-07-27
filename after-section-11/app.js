0fb3ab6b7b202f910c50cd8bdfbece8e1136ba0b
Ranjan <ranjan.kumar@publicissapient.com>
2023-07-27 20:16:08 +0530

    pug

--- after-section-11/app.js
+++ after-section-11/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -15,6 +16,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const app = express();
 
 app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "views"))
 // 1) GLOBAL MIDDLEWARES
 // Set security HTTP headers
 app.use(helmet());
@@ -56,7 +58,8 @@ app.use(
 );
 
 // Serving static files
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Test middleware
 app.use((req, res, next) => {
@@ -66,6 +69,19 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+app.get("/", (req, res) => {
+  res.status(200).render("base")
+});
+app.get("/overview", (req, res) => {
+  res.status(200).render("overview", {
+    title: "All Tours"
+  })
+})
+app.get("/tour", (req, res) => {
+  res.status(200).render("overview", {
+    title: "The forest hiker tour"
+  })
+})
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
