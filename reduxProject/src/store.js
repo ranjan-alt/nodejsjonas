93cec99939890f4f912008ea7aca51444de8f8be
Ranjan <ranjan.kumar@publicissapient.com>
2023-09-28 01:38:14 +0530

    redux thunk

--- reduxProject/src/store.js
+++ reduxProject/src/store.js
@@ -1,4 +1,4 @@
-import { combineReducers, createStore } from "redux"
+import { combineReducers, createStore, applyMiddleware } from "redux"
 import accountReducer from "./features/accounts/accountSlice"
 import customerReducer from "./features/customers/customerSlice"
 
@@ -8,7 +8,7 @@ const rootReducer = combineReducers({
     customer: customerReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 
 export default store
