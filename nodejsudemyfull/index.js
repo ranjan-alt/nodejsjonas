dea81de8895433c65ac1300b4649191d04c9ae26
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2024-03-22 02:46:33 +0000

    call back

--- nodejsudemyfull/index.js
+++ nodejsudemyfull/index.js
@@ -1,16 +1,23 @@
 const fs = require("fs");
 
-console.log("script started");
-try {
-  const text = fs.readFileSync("./txt/main.txt", "utf-8");
-  // this read filesync is a synchronous in nature
-  console.log("file read succesffuly");
+// console.log("script started");
+// try {
+//   const text = fs.readFileSync("./txt/main.txt", "utf-8");
+//   // this read filesync is a synchronous in nature
+//   console.log("file read succesffuly");
 
-  console.log(text);
-} catch (error) {
-  console.error("error reading file", error);
-}
-console.log("script ended");
+//   console.log(text);
+// } catch (error) {
+//   console.error("error reading file", error);
+// }
+// console.log("script ended");
 
-const textOut = `this is new line ${text} .\n created on ${Data.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
+// const textOut = `this is new line ${text} .created on ${Data.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+fs.readFile("./txt/main.txt","utf-8", (err,data)=>{
+  console.log(data)
+})
+
+
+
