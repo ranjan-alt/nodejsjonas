79fce8100754fd05c36e8a3ae70c08679ee8b6ce
Ranjan <ranjan.kumar@publicissapient.com>
2024-03-23 23:46:29 +0530

    express

--- nodejsudemyfull/index.js
+++ nodejsudemyfull/index.js
@@ -25,12 +25,36 @@ fs.readFile("./txt/main.txt", "utf-8", (err, data) => {
 
 // Server
 
+const data = fs.readFileSync(`${__dirname}/txt/data.json`, "utf-8")
+const productData = JSON.parse(data)
+
+
 const server = http.createServer((req, res) => {
- console.log(req.url)
-  res.send("hello form the server");
+  console.log(req.url)
+  const pathName = req.url;
+  if (pathName == "/" || pathName == "/overview") {
+    res.end("This is overview")
+  } else if (pathName == "/product") {
+    res.end("this is product")
+  } else if (pathName == "/api") {
+    // res.end("API")
+    // sned json data to client into some readble format
+    //first thing is to reaad the file 
+    res.writeHead(200, { "content-type": "application/json" })
+    res.end(data)
+
+  } else {
+    res.writeHead(404, {             //to inform the broswr or client about reciveing the response one example is content type
+      "Content-type": "text/html",
+      "my-own-header": "Hello world",
+      "name": "ranjan"
+    })
+    res.end("<h1>Page not found</h1>");
+  }
+
 });
 
-server.listen(8000, "127.0.0.0", () => {
+server.listen(8000, "127.0.0.1", () => {
   console.log("Listning on port 8000");
 });
 
