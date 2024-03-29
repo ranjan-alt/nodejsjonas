116cdf0224fc9fc01f09ade0fed1b4d3a207f4c4
Ranjan <ranjan.kumar@publicissapient.com>
2024-03-29 14:22:23 +0530

    user

--- nodejsudemyfull/app.js
+++ nodejsudemyfull/app.js
@@ -101,6 +101,42 @@ const deleteTour = (req, res) => {
     })
 }
 
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined"
+    })
+}
+
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yte defined"
+    })
+}
+
+const getUser = () => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined"
+    })
+}
+
+
+const updateUser = () => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined"
+    })
+}
+
+const deleteUser = () => {
+    res.status(500).json({
+        status: "Error",
+        message: "This route is not yet defined"
+    })
+}
+
 // app.get("/api/v1/tours", getAllTours)
 // app.get("/api/v1/tours/:id", getTour)
 // app.post("/api/v1/tours", createTour)
@@ -110,6 +146,10 @@ const deleteTour = (req, res) => {
 app.route("/api/v1/tours").get(getAllTours).post(createTour)
 app.route("/api/v1/tours/:id").get(getTour).patch(updateTour).delete(deleteTour)
 
+
+//user routes
+app.route("/api/v1/users").get(getAllUsers).post(createUser)
+app.route("/api/v1/users/:id").get(getUser).patch(updateUser).delete(deleteUser)
 
 const port = 3000
 app.listen(port, () => {
