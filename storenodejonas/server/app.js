839f36b712851831ecb0cf2fcc673b98068a568c
Ranjan <ranjan.kumar@publicissapient.com>
2024-07-04 15:35:13 +0530

    learning

--- storenodejonas/server/app.js
+++ storenodejonas/server/app.js
@@ -1,15 +1,26 @@
 require("dotenv").config()
-
-
+// require("express-async-errors")
+// var request = require('request');
+const request = require("request")
 
 const express = require("express")
+
 const app = express()
+
+const connectDb = require("./connectDb")
+const productRouter = require("./routes/products")
 
 const notFound = require("./middleware/not-found")
 const errorHandlerMiddleware = require("./middleware/error-handler")
 
 //middleware
 app.use(express.json())
+
+
+//products route
+
+'use strict';
+
 
 
 //routes
@@ -19,16 +30,19 @@ app.get("/", (req, res) => {
 
 
 
-//products route
+app.use("/api/v1/products", productRouter)
 
 
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
+
+
 const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
+        await connectDb()
         app.listen(port, console.log(`app is running on ${port}`))
     } catch (error) {
         console.log(error)
